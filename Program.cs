8e6c7290c6163a189adc704cacdea648b4936e26
Clovis Alberto <clovis10alberto@hotmail.com>
2019-11-13 11:55:42 -0300

    Acrescentada a mensagem 'Bom dia'

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Projeto_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
