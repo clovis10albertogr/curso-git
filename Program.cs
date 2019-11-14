90ba4ab8ae8b29a01ac81f68884e1f23ce0f818a
Clovis Alberto <clovis10alberto@hotmail.com>
2019-11-14 12:13:02 -0300

    Acrescentada a mensagem 'Como voce esta hoje?'

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_Teste
         {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
