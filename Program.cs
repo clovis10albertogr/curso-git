1fff67f89285d488064c5bba761562bbfb4bcbc6
Clovis Alberto <clovis10alberto@hotmail.com>
2019-11-16 11:45:57 -0300

    Acrescentado tracos

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace Projeto_Teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------");
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
